870ef48c10120e4316f43952827c10a358866025
kevinmvc20 <kevinmvc20@gmail.com>
2022-01-05 17:18:53 -0400

    subiendo un trigger

---
+++ database/migrations/2022_01_05_211034_create_trigger.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrigger extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('
+        CREATE TRIGGER cantidad_compras AFTER INSERT ON `orden_compras` FOR EACH ROW
+            BEGIN
+                UPDATE compras SET compras.cantidadtotal=compras.cantidadtotal+1 WHERE
+                compras.id = NEW.compra_id;
+            END
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP TRIGGER `cantidad_compras`');
+    }
+}
