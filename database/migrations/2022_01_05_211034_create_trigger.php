6e552b33b8d7e03158f0b521ca9bc64e4314e8f2
kevinmvc20 <kevinmvc20@gmail.com>
2022-01-05 17:54:29 -0400

    subiendo trigger cuarto intento

--- database/migrations/2022_01_05_211034_create_trigger.php
+++ database/migrations/2022_01_05_211034_create_trigger.php
@@ -14,7 +14,7 @@ class CreateTrigger extends Migration
     public function up()
     {
         DB::unprepared('
-        CREATE TRIGGER cantidad_compras AFTER INSERT ON orden_compras FOR EACH ROW
+        CREATE TRIGGER cantidad_compras AFTER INSERT ON "orden_compras" FOR EACH ROW
             BEGIN
                 UPDATE compras SET compras.cantidadtotal=compras.cantidadtotal+1 WHERE
                 compras.id = NEW.compra_id;
